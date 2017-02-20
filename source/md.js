56f129084b126992e45a95657eef006d319c12bf
rhaldkhein <undyingmind@gmail.com>
2017-02-20 13:27:50 +0400

    v0.4.0

--- source/md.js
+++ source/md.js
@@ -83,7 +83,7 @@ function resolveSchemaOptions(options) {
 
 // Return the current version.
 exports.version = function() {
-	return 'v0.3.3';//version
+	return 'v0.4.0';//version
 };
 
 // Export class Collection.
