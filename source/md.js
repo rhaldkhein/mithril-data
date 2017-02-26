a5eaf848a592bf8b9491b8c0f2b41db230fc9c25
rhaldkhein <undyingmind@gmail.com>
2017-02-26 10:31:07 +0400

    v0.4.1
    - Fixed bug #6
    - Added `assign` method to State

--- source/md.js
+++ source/md.js
@@ -83,7 +83,7 @@ function resolveSchemaOptions(options) {
 
 // Return the current version.
 exports.version = function() {
-	return 'v0.4.0';//version
+	return 'v0.4.1';//version
 };
 
 // Export class Collection.
